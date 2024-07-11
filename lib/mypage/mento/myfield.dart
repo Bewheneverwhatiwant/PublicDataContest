4ea4f808f48b065f0513e4c8ca4e79155285c538
nayeong lee <lny021102@gmail.com>
2024-07-11 19:52:17 +0900

    [ADD] 멘토 내 멘토링분야 추가API 연동 성공

--- lib/mypage/mento/myfield.dart
+++ lib/mypage/mento/myfield.dart
@@ -86,6 +86,33 @@ class _MyFieldState extends State<MyField> {
     return prefs.getString('accessToken');
   }
 
+  void _showCategoryDialog() async {
+    final categories = await _fetchCategories();
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return CategoryDialog(
+          categories: categories,
+          onSuccess: () {
+            _fetchMentorCategoryNames();
+          },
+        );
+      },
+    );
+  }
+
+  Future<List<dynamic>> _fetchCategories() async {
+    final String apiServer = dotenv.env['API_SERVER'] ?? '';
+    final response =
+        await http.get(Uri.parse('$apiServer/api/category/categoryList'));
+
+    if (response.statusCode == 200) {
+      return json.decode(response.body);
+    } else {
+      throw Exception('Failed to load categories');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return _isLoading
@@ -116,7 +143,7 @@ class _MyFieldState extends State<MyField> {
                 ),
               ),
               TextButton(
-                onPressed: widget.showCategoryDialog,
+                onPressed: _showCategoryDialog,
                 style: TextButton.styleFrom(
                   backgroundColor: GlobalColors.mainColor,
                   shape: RoundedRectangleBorder(
@@ -135,5 +162,134 @@ class _MyFieldState extends State<MyField> {
               ),
             ],
           );
+  }
+}
+
+class CategoryDialog extends StatefulWidget {
+  final List<dynamic> categories;
+  final VoidCallback onSuccess;
+
+  const CategoryDialog({
+    Key? key,
+    required this.categories,
+    required this.onSuccess,
+  }) : super(key: key);
+
+  @override
+  _CategoryDialogState createState() => _CategoryDialogState();
+}
+
+class _CategoryDialogState extends State<CategoryDialog> {
+  List<dynamic>? subCategories;
+  String? selectedCategoryName;
+
+  void _showSubCategories(List<dynamic> subCategories, String categoryName) {
+    setState(() {
+      this.subCategories = subCategories;
+      this.selectedCategoryName = categoryName;
+    });
+  }
+
+  Future<void> _addSubCategory(int subCategoryId) async {
+    final String apiServer = dotenv.env['API_SERVER'] ?? '';
+    final String? accessToken = await _getAccessToken();
+
+    if (accessToken == null) {
+      print('AccessToken not found');
+      return;
+    }
+
+    final response = await http.post(
+      Uri.parse('$apiServer/api/category/subCategory/mentor'),
+      headers: {
+        'Authorization': 'Bearer $accessToken',
+        'Content-Type': 'application/json',
+      },
+      body: json.encode({
+        'subCategoryId': [subCategoryId],
+      }),
+    );
+
+    if (response.statusCode == 200) {
+      _showSuccessDialog();
+    } else {
+      print('Failed to add subcategory');
+    }
+  }
+
+  Future<void> _showSuccessDialog() async {
+    await showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('알림'),
+          content: const Text('내 멘토링 분야가 등록되었습니다!'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+                Navigator.of(context).pop();
+                widget.onSuccess();
+              },
+              child: const Text('확인'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  Future<String?> _getAccessToken() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getString('accessToken');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: Text(selectedCategoryName ?? '카테고리 선택'),
+      content: SingleChildScrollView(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            if (subCategories == null)
+              ...widget.categories.map((category) {
+                return ListTile(
+                  title: Text(category['categoryName']),
+                  onTap: () => _showSubCategories(
+                      category['subCategories'], category['categoryName']),
+                );
+              }).toList()
+            else
+              ...subCategories!.map((subCategory) {
+                return ListTile(
+                  title: Text(subCategory['subCategoryName']),
+                  trailing: ElevatedButton(
+                    onPressed: () =>
+                        _addSubCategory(subCategory['subCategoryId']),
+                    child: const Text('추가하기'),
+                  ),
+                );
+              }).toList()
+          ],
+        ),
+      ),
+      actions: [
+        if (subCategories != null)
+          TextButton(
+            onPressed: () {
+              setState(() {
+                subCategories = null;
+                selectedCategoryName = null;
+              });
+            },
+            child: const Text('뒤로가기'),
+          ),
+        TextButton(
+          onPressed: () => Navigator.of(context).pop(),
+          child: const Text('닫기'),
+        ),
+      ],
+    );
   }
 }
